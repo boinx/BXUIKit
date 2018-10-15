90b8868f5b8c22c1ca90ef5be557b38a56a2b9bf
Stefan Fochler <istefo@me.com>
2018-10-15 15:41:24 +0200

    Fix curl password promt
    There is no password, only the user name

--- download_bxswiftutils.sh
+++ download_bxswiftutils.sh
@@ -6,13 +6,13 @@ function download_for_platform
 {
   _platform=$1
   pushd "Frameworks/${_platform}"
-  curl -OL --user "${GH_TOKEN}" "https://github.com/boinx/BXSwiftUtils/releases/download/${_tag}/BXSwiftUtils-${_platform}.framework.zip"
+  curl -OL --fail --user "${GH_TOKEN}:" "https://github.com/boinx/BXSwiftUtils/releases/download/${_tag}/BXSwiftUtils-${_platform}.framework.zip"
   unzip -o *.zip
   popd
 }
 
 # Find out the latest tag that was released on Github, because the tag needs to be inserted into the download URls.
-_tag=$(curl -s --user "${GH_TOKEN}" https://api.github.com/repos/boinx/BXSwiftUtils/releases/latest | jq -r '.tag_name')
+_tag=$(curl -s --fail --user "${GH_TOKEN}:" https://api.github.com/repos/boinx/BXSwiftUtils/releases/latest | jq -r '.tag_name')
 
 # Download frameworks for both macOS and iOS.
 download_for_platform "macOS"
