a0e20b75129086b2e67b295a724cb3370ad34f61
Stefan Fochler <istefo@me.com>
2018-10-15 07:05:25 +0200

    Initial Commit

---
+++ BXUIKit/BXUIKit.h
@@ -0,0 +1,19 @@
+//
+//  BXUIKit.h
+//  BXUIKit
+//
+//  Created by Stefan Fochler on 15.10.18.
+//  Copyright © 2018 Boinx Software Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BXUIKit.
+FOUNDATION_EXPORT double BXUIKitVersionNumber;
+
+//! Project version string for BXUIKit.
+FOUNDATION_EXPORT const unsigned char BXUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BXUIKit/PublicHeader.h>
+
+
