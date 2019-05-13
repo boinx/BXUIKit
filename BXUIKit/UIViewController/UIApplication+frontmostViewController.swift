8fd5360faae7c3b7cf104f6f351fa7dbdb1a96f0
Peter Baumgartner <peter@boinx.com>
2019-05-13 10:27:12 +0200

    Moved UIApplication+frontmostViewController to BXUIKit

---
+++ BXUIKit/UIViewController/UIApplication+frontmostViewController.swift
@@ -0,0 +1,64 @@
+//**********************************************************************************************************************
+//
+//  UIApplication+frontmostViewController.swift
+//	Returns the frontmost UIViewController that is not a UIAlertController
+//  Copyright ©2019 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+#if os(iOS)
+
+import UIKit
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension UIApplication
+{
+	/// Returns the frontmost UIViewController that is not a UIAlertController
+
+    static var frontmostViewController: UIViewController?
+    {
+        return UIApplication.shared.keyWindow?.rootViewController?.frontmostViewController
+    }
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension UIViewController
+{
+    /// Returns the frontmost UIViewController that is not a UIAlertController
+
+    var frontmostViewController: UIViewController?
+    {
+        if let navigationController = self as? UINavigationController
+        {
+            return navigationController.topViewController?.frontmostViewController
+        }
+        else if let tabBarController = self as? UITabBarController
+        {
+            return tabBarController.selectedViewController?.frontmostViewController
+        }
+        else if let presentedViewController = presentedViewController
+        {
+            return presentedViewController.frontmostViewController
+        }
+        else if self is UIAlertController
+        {
+            return nil
+        }
+        else
+        {
+            return self
+        }
+    }
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+#endif
