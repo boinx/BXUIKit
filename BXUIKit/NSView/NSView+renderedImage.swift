fbe0f06bc3f11441b6090cb262f9a47302437b5b
Peter Baumgartner <peter@boinx.com>
2020-08-05 19:11:49 +0200

    Convenience function to render an NSImage from a NSView

---
+++ BXUIKit/NSView/NSView+renderedImage.swift
@@ -0,0 +1,100 @@
+//**********************************************************************************************************************
+//
+//  NSView+renderedImage.swift
+//	Functions to render an NSView to an image
+//  Copyright ©2020 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+#if os(macOS)
+
+import AppKit
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+extension NSView
+{
+
+	/// Helper to get the current CGContext
+	
+	public static var currentContext:CGContext?
+	{
+		guard let ptr = NSGraphicsContext.current?.graphicsPort else { return nil }
+		return Unmanaged<CGContext>.fromOpaque(ptr).takeUnretainedValue()
+	}
+
+
+	/// Renders an image from this view
+	
+	public var renderedImage:NSImage
+	{
+		let image = NSImage(size:self.bounds.size)
+		image.lockFocus()
+		
+		if let context = Self.currentContext
+		{
+			self.layer?.render(in: context)
+		}
+		
+		image.unlockFocus()
+		return image
+	}
+	
+	
+	/// Create a compound image from multiple views
+	
+	public static func renderedImage(from views:[NSView]) -> NSImage
+	{
+		// Get enclosing bounds of all views
+		
+		var frame = CGRect.zero
+		for view in views
+		{
+			frame = NSUnionRect(frame, view.frame)
+		}
+		
+		// Create an image of this size and draw views into the image
+		
+		let image = NSImage(size:frame.size)
+		image.lockFocus()
+		
+		for view in views
+		{
+			let rect = view.frame
+			view.renderedImage.draw(in:rect)
+		}
+
+		image.unlockFocus()
+		
+		return image
+	}
+	
+	
+//----------------------------------------------------------------------------------------------------------------------
+
+
+	/// Returns the enclosing rect for a list of subviews
+	
+	public func enclosingRect(for subviews:[NSView]) -> CGRect
+	{
+		var enlosingFrame = CGRect.zero
+		
+		for subview in subviews
+		{
+			let frame = self.convert(subview.bounds, from:subview)
+			enlosingFrame = NSUnionRect(enlosingFrame, frame)
+		}
+		
+		return enlosingFrame
+	}
+	
+	
+//----------------------------------------------------------------------------------------------------------------------
+
+
+}
+
+#endif
